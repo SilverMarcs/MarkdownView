d3afd9b38d4dc1aa204638c52f5e4fed868c25c3
Zabir Raihan <zabirraihan@icloud.com>
2024-12-12 09:32:50 +0600

    reduce inline code opacity

--- Sources/MarkdownView/Extension/Color++.swift
+++ Sources/MarkdownView/Extension/Color++.swift
@@ -5,6 +5,6 @@
 import SwiftUI
 
 extension Color {
-  public static let code = Color.gray.opacity(0.3)
+  public static let code = Color.gray.opacity(0.2)
   public static let symbolLink = Color.gray.opacity(0.3)
 }
